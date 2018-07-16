57122221d8ecbd73707eef35daa26489a91485b2
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-07-16 23:06:53 +0000

    Projeto da aula 2 - Criando nossa Lista

---
+++ ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,71 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    // [item][item][item][item][item]
+    //                                ^
+    //                                 `- _proximaPosicao
+
+
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+        
+        public ListaDeContaCorrente(int capacidadeInicial = 5)
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void MeuMetodo(string texto = "texti padrao", int numero = 5)
+        {
+
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+            Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if(_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            Console.WriteLine("Aumentando capacidade da lista!");
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for(int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+                Console.WriteLine(".");
+            }
+
+            _itens = novoArray;
+        }
+
+
+
+    }
+}
